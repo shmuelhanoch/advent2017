23b59674f90a69ef3da51b6f370a03061a3c46d9
Shmuel Hanoch <shmuelhanoch@gmail.com>
2017-12-27 18:05:47 +0200

    essage for your changes. Lines starting fixed and added part two

--- Day1.hs
+++ Day1.hs
@@ -1,9 +1,12 @@
-solve :: (Num a, Eq a) => [a] -> a
-solve = sum . fst . unzip . filter (\(x, y) -> x==y) . circPairs
-    where
-        circPairs :: [a] -> [(a, a)]
-        circPairs l = zip l ((tail l) ++ [head l])
-
+solve :: (Num a, Eq a) => Int -> [a] -> a
+solve gap xs = sum $ zipWith (\x y -> if x==y then x else 0) xs (drop gap xs ++ take gap xs)
 
 main :: IO ()
-main = getLine >>= print . solve . (map (read . (:[])))
+main = do
+    xs <- fmap (map (read . (:[]))) getLine
+    
+    --part one:
+    print $ solve 1 xs
+    
+    -- part two:
+    print $ solve ((length xs) `quot` 2) xs
